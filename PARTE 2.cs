cb1f651fcb1c3cc52a5f645f9733bed9f0862d23
Jlmossj <jmolinao1@miumg.edu.gt>
2025-02-10 11:49:22 -0600

    PARTE 2 DEL LABORATORIO 2

---
+++ PARTE 2.cs
@@ -0,0 +1,22 @@
+﻿double num1;
+double num2;
+
+while (true)
+{
+    Console.WriteLine("Digite el primer numero: ");
+    num1 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite el segundo numero: ");
+    num2 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("La suma de los dos numeros ingresados es: " + (num1 + num2));
+    Console.WriteLine("La resta de los dos numeros ingresados es: " + (num1 - num2));
+    Console.WriteLine("La multiplicacion de los dos numeros ingresados es: " + (num1 * num2));
+    Console.WriteLine("La division de los dos numeros ingresados es: " + (num1 / num2));
+
+    Console.WriteLine("Desea continuar? (s/n)");
+    if (Console.ReadLine() == "n")
+    {
+        break;
+    }
+}
