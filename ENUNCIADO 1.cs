10daa4921aee087c00f8d58d5e16535c1c4bb4a8
Jlmossj <jmolinao1@miumg.edu.gt>
2025-02-11 12:14:18 -0600

    ENUNCIADO 1 - CLASIFICACION DE EDAD

---
+++ ENUNCIADO 1.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Ingrese su edad");
+int num1 = int.Parse(Console.ReadLine());
+
+if (num1 <= 12)
+{
+    Console.WriteLine(" Usted es un niño");
+}
+else if (num1 > 12 && num1 < 18)
+{
+    Console.WriteLine(" Usted es un adolescente");
+}
+else if (num1 >= 18 && num1 < 60)
+{
+    Console.WriteLine(" Usted es un adulto");
+}
+else
+{
+    Console.WriteLine(" Usted es un adulto mayor");
+}
+Console.WriteLine("");
+Console.WriteLine("En este caso utilice el If-else ya que me permite evaluar los datos ingresado por medio de rangos y asi poder determinar la edad de la persona");
